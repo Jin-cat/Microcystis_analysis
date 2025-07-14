2857a9adf67f045a76b8dbbfd58830415c427f00
Jina Kim <122256622+Jin-cat@users.noreply.github.com>
2025-07-14 15:16:07 +0900

    Update 1_annotation_and_peppan.sh

--- code/1_annotation_and_peppan.sh
+++ code/1_annotation_and_peppan.sh
@@ -9,8 +9,8 @@ exec > >(tee -i PEPPAN_core_gene_analysis_log.txt)
 exec 2>&1
 
 # === [User-defined directory paths] ===
-INPUT_DIR="/data/Microcystis/0_rawdata"       # Raw genome input (.fna)
-OUTPUT_DIR="/data/Microcystis"                # Base output directory
+INPUT_DIR="/data/Microcystis/0_rawdata"       
+OUTPUT_DIR="/data/Microcystis"                
 
 # === [Genome annotation using Prokka] ===
 echo "Running Prokka for genome annotation"
